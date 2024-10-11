32fdb4ac6dbee90ae197a91e25030a573f52cc02
SohelShahid117 <sohelshahid09@gmail.com>
2024-10-11 22:26:11 +0600

    from w3school cpp tutorial home to comments part done

---
+++ home.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //C++ Tutorial-Home,Introduction,Getting Started
+    cout<<"hello world\n";
+    cout<<"how r u\n";
+
+
+    return 0;
+}
+/*
+1.C++ was developed by Bjarne Stroustrup, as an extension to the C language.
+2.C++ is fun and easy to learn!
+3.As C++ is close to C, C# and Java, it makes it easy for programmers to switch to C++ or vice versa.
+
+4.The main difference between C and C++ is that C++ support classes and objects, while C does not.
+*/
---
+++ syntax.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //C++ Syntax,Statements,Output (Print Text),Output Numbers,New Lines,Comments
+
+
+    cout<<"hello\n";
+    cout<<"enjoy CPP\n";
+    cout<<"now i am learning CPP\n";
+
+
+    cout<<5<<"\n";
+    cout<<4+5<<"\n";
+    cout<<4-5<<"\n";
+    cout<<4*5<<"\n";
+    cout<<4/5<<"\n";
+    cout<<4%5<<"\n";
+
+    cout<<"i am programmer \t \"sohel\" "<<endl;
+    cout<<"i enjoy programming as like game\n";
+
+
+    return 0;
+}
+
+/*
+1.
+
+*/
